0374748bf37e06cf8ec048bc088a6287464470bf
Hugo <45490945+hugodehaan@users.noreply.github.com>
2021-09-13 12:04:11 +0000

    4 ballen

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-var x, y, vx, vy x2, y2;
+class Ball{
+  
+  constructor(x, y, w, h, vx, vy, c) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.vx = vx;
+    this.vy = vy;
+    this.c = c;
+  }
+
+  draw() {
+    fill(this.c);
+    ellipse(this.x,this.y,50,50);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if(this.x < 25 || this.x >= 475){
+      this.vx = this.vx * -1;
+    }
+
+    if(this.y < 25 || this.y >= 375){
+      this.vy = this.vy * -1;
+    }
+  }
+}
+
+var ball1, ball2, ball3;
 
 function setup() {
 	createCanvas(500, 400);
 
-  x= 0;
-  x2=50;
-  y2=50;
-  y= 200;
-  vx = 8;
-  vy=5;
+  ball1 = new Ball(30,200,50,50,10,5,"red");
+
+  ball2 = new Ball(100,100,50,50,10,3,"green");
+
+  ball3 = new Ball(200,150,50,50,5,-1,"pink");
+
+  ball4 = new Ball(70,150,50,50,1,-5,"purple");
+
 }
-
-
 
 function draw() {
-	background(225);
-  drawball()
-	
-function drawball(){
-ellipse(x,y,80,80)
-  x=x+vx;
-  y=y+vy;
-
-    if (y<=0||y>=400){
-    vy=vy*-1;
-  }
-
-	
-
-  if(x <= 0 || x>=500){
-    vx=vx*-1;
-  }
-
-
+	background(225);  
+  
+  ball1.draw();
+  ball2.draw();
+  ball3.draw();
+  ball4.draw();
 }
-}
+
